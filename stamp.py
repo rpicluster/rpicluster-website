7ccdc5d026a72fb4a6bd81a887f4e3163e33c81a
Derek Dang <dqdang17@gmail.com>
2018-04-25 13:33:05 -0700

    Add additional changes

--- stamp.py
+++ stamp.py
@@ -2,6 +2,9 @@ import sys, os
 
 img = "Server.img"
 
+password = input("Enter a password.")
+# password = ''.join(format(ord(x), 'b') for x in password)
+
 def find_point(magic_num, img):
     #search .img for magic_num and return fd at that point
     pos = 0
@@ -19,7 +22,7 @@ def find_point(magic_num, img):
         else:
             pos = 0
         char = ord(fd.read(1))
-
+    null += fd.read(len(password))
     second += fd.read()
     return (first, second)
 
@@ -28,8 +31,10 @@ print("calling find point on {}".format(img))
 parts = find_point("0100010001000001010101110100000101000101", img)
 print("after find point")
 fd = open(img, 'w')
+
 fd.write(parts[0])
-fd.write(" Alecs-AP password")
+fd.write(password)
 fd.write(parts[1])
+
 fd.close()
 os.system("zip Server.zip Server.img")
